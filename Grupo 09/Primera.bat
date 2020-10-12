9401d708ce1d9ccd4be8c59f3248ad892597c26d
SantiagoAlbarracin <snalbarracin@live.com>
2020-10-12 16:46:47 -0300

    Corrección Entrega

--- Grupo 09/Primera.bat
+++ Grupo 09/Primera.bat
@@ -2,14 +2,14 @@ C:\GnuWin32\bin\flex Lexico.l
 pause
 c:\GnuWin32\bin\bison -dyv Sintactico.y
 pause
-c:\MinGW\bin\gcc.exe lex.yy.c y.tab.c -o TPFinal.exe
+c:\MinGW\bin\gcc.exe lex.yy.c y.tab.c -o Primera.exe
 pause
 pause
-TPfinal.exe prueba.txt
+Primera.exe prueba.txt
 
 
 del lex.yy.c
-del TPFinal.exe
+del Primera.exe
 del y.tab.c
 del y.tab.h
 pause
